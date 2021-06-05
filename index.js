f3c7ad1bf1940793bf198e03f31a28215165f7fe
Hasar Ali <hasar.ali@protonmail.com>
2021-06-05 04:27:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,17 +17,29 @@ class Cat extends Animal {
   }
   
   speak() {
+<<<<<<< HEAD
     return super.speak('meow')
+=======
+    super('meow')
+>>>>>>> e525a1011e5ef479ecac4ba291e11ac1b01d424d
   }
 }
 
 class Dog extends Animal {
   constructor(name, sex) {
+<<<<<<< HEAD
     super(name, sex)
   }
   
   speak() {
     return super.speak('woof')
+=======
+    return super(name, sex)
+  }
+  
+  speak() {
+    return super('woof')
+>>>>>>> e525a1011e5ef479ecac4ba291e11ac1b01d424d
   }
 }
 
@@ -38,7 +50,11 @@ class Bird extends Animal {
   
   speak() {
     if (this.sex === 'male') 
+<<<<<<< HEAD
       return `It's me! ${this.name}, the parrot!`
+=======
+      return `It's me! ${this.name} the parrot`
+>>>>>>> e525a1011e5ef479ecac4ba291e11ac1b01d424d
     else
       return `${this.name} says squawk!`
   }
